9fe82f9220ca74db86f3b8964477a8974d5e09eb
MauricioGonzalezCastelazo <127879086+MauricioGonzalezCastelazo@users.noreply.github.com>
2024-03-18 23:00:57 -0600

    Add files via upload

--- API.py
+++ API.py
@@ -2,13 +2,14 @@ import requests
 import json 
 from base64 import b64encode
 import urllib3
+import io 
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 class API: 
     def __init__(self, ip): 
-        self.user = 'admin'
-        self.passw = 'admin'
+        self.user = 'wazuh-wui'
+        self.passw = 'wazuh-wui'
         self.url = f'https://{ip}:55000/'
         self.auth = f'{self.user}:{self.passw}'.encode()
         self.headers = {
@@ -16,10 +17,9 @@ class API:
             'Content-Type': 'application/json'
         }
         token_url = self.url+'security/user/authenticate'
-        token = self.get_response("POST", token_url, self.headers)
-        print(token)
-        if token and "data" in token and "token" in token["data"]:
-            self.headers['Authorization'] = f'Bearer {token["data"]["token"]}'
+        self.token = self.get_response("POST", token_url, self.headers)
+        if self.token and "data" in self.token and "token" in self.token["data"]:
+            self.headers['Authorization'] = f'Bearer {self.token["data"]["token"]}'
         else:
             print("No se pudo obtener el token")
 
@@ -39,7 +39,77 @@ class API:
         except Exception as e:
             print(f'Ocurrió un error: {e}')
             return None 
+        
+    def addDecoderRule(self, type, fileName, Content): 
+        file = io.BytesIO(Content.encode('utf-8'))
+        files = {'file' : ('pruebas.xml', file)}
+        endpoint = f'{type}/files/'
+        url = self.url+endpoint+fileName
+        endpoint = endpoint+fileName
+        new_header = {
+            'Authorization' : self.headers['Authorization'], 
+            'Content-Type': 'application/octet-stream'
+        }
+        try: 
+            response = requests.put(url, headers = new_header, files=files, verify = False)
+            response.raise_for_status()
+            print("Se ha modificado con exito")
+            return response.json()
+        except Exception as e: 
+            print(f"No se pudo modificar {e}")
 
-            
+
+    def updateDecoderRule(self, type, fileName, Content): 
+        file = io.BytesIO(Content.encode('utf-8'))
+        files = {'file' : ('pruebas.xml', file)}
+        endpoint = f'{type}/files/'
+        url = self.url+endpoint+fileName+'?overwrite=true'
+        endpoint = endpoint+fileName
+        new_header = {
+            'Authorization' : self.headers['Authorization'], 
+            'Content-Type': 'application/octet-stream'
+        }
+        try: 
+            response = requests.put(url, headers = new_header, files=files, verify = False)
+            response.raise_for_status()
+            print("Se ha modificado con exito")
+            return response.json()
+        except Exception as e: 
+            print(f"No se pudo modificar {e}")
+
+        
+    def get(self, type, fileName): 
+        endpoint = f'{type}/files/'
+        url = self.url+endpoint+fileName
+        endpoint = endpoint+fileName
+        new_header = {
+            'Authorization' : self.headers['Authorization'], 
+            'Content-Type' : 'application/json'
+        }
+        try: 
+            response = requests.get(url, headers = new_header, verify = False)
+            response.raise_for_status()
+            print(f"Se ha obtenido la informacin con exito {response.json()}")
+            return response.json()
+        except Exception as e: 
+            print(f"No se pudo modificar {e}")
+
+
+xml = """
+<decoder name="mi-decoder10">
+    <program_name>mi decoder10</program_name>
+</decoder>
+"""
+        
+#xml = """
+# <rule id="100011" level="5">
+#    <match>Menor 10K</match>
+#    <description>Registro creado de manera exitosa</description>
+#  </rule>
+#"""
+
+
 
 api = API("192.168.0.158")
+print(api.updateDecoderRule('decoders', 'pruebas.xml',xml))
+#api.get('decoders', 'pruebas.xml')
--- decoder_rules.py
+++ decoder_rules.py
@@ -2,7 +2,7 @@ from API import API
 
 class Decoder: 
     def __init__(self): 
-        pass
+        self.api = API("192.168.0.158")
 
     def read_log(self, txt): 
         pass
@@ -10,9 +10,26 @@ class Decoder:
     def generate_decoder(self): 
         pass
 
+    def addDecoder(self, type = "decoders", fileName="", xml=""): 
+        response = self.api.addDecoderRule(type, fileName=fileName, Content=xml)
+        print(response)
+
+    def updateDecoder(self, type = "decoders", fileName="", xml=""): 
+        response = self.api.updateDecoderRule(type, fileName=fileName, Content=xml)
+        print(response)
+
 class Rule: 
     def __init__(self): 
         pass
 
     def create_rule(self): 
         pass
+
+    def addRule(self, type = "rules", fileName="", xml=""): 
+        response = self.api.addDecoderRule(type, fileName=fileName, Content=xml)
+        print(response)
+
+    def updateRule(self, type = "rules", fileName="", xml=""): 
+        response = self.api.updateDecoderRule(type, fileName=fileName, Content=xml)
+        print(response)
+
